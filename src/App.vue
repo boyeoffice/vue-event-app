088b14ada4287d0919cf2e9d12fa634fae29614d
Olakunle Boye <kunlexzy@gmail.com>
2020-03-07 02:44:52 +0100

    testing typescript

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,50 @@
 <template>
-  <div>
-    <navbar />
+  <div id="app">
+    
   </div>
 </template>
 
 <script lang="ts">
-  import Navbar from './components/Navbar'
-  import Vue from 'vue'
-  export default Vue.extend({
-    name: 'App',
-    components: {
-      Navbar
+import Vue from "vue";
+import { User } from "@/types";
+// import HelloWorld from "./components/HelloWorld.vue";
+
+export default Vue.extend({
+  name: "App" as string,
+  components: {
+    // HelloWorld
+  },
+  data() {
+    return {
+      user: {} as User // Declare reactive data as type User
+    };
+  },
+  mounted() {
+    this.user = {
+      firstName: "Olakunle",
+      lastName: "Boye",
+      twitterHandle: "@dev_boye",
+      location: {
+        city: "Ikeja",
+        state: "Lagos"
+      }
+    };
+  },
+  computed: {
+    fullName(): string {
+      return `${this.user.firstName} ${this.user.lastName}`;
     }
-  })
+  }
+});
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
